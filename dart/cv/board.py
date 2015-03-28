7d41cd39b0564a049a154bf1833ad702a01e988a
Olav Vatne <olavvatne@gmail.com>
2015-03-28 16:07:52 +0100

    Clean up todos and output from board.py

--- dart/cv/board.py
+++ dart/cv/board.py
@@ -14,9 +14,7 @@ class Board:
         With the image supplied as argument, a shape description containing the board outline,
         and bulleye coordinates.
         '''
-        #TODO: Find outline
-        #TODO: Find bullseye center
-        #TODO: Create a shape dscription and return it
+
         blurred = cv2.GaussianBlur(image,(5,5),0)
         #outline = self._outline_segmentation(blurred)
         red_mask, green_mask = self._color_difference_segmentation(blurred)
@@ -24,15 +22,7 @@ class Board:
         green_scores = self._create_score_areas(green_mask)
         center = self._identify_bullseye(red_scores)
         outline = self._fit_ellipse(red_scores)
-        cv2.circle(blurred, center, 5, (0, 255,100), thickness=2)
-        unified = []
-        unified.append(outline)
-        cv2.drawContours(blurred,unified,-1,(255,0,0),thickness=2)
-        #cv2.imshow("red",red_mask)
-        #cv2.imshow("green",green_mask)
-        cv2.imshow("abc", blurred)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        return center, outline, red_scores, green_scores
 
 
 
@@ -41,34 +31,17 @@ class Board:
 
     def _create_score_areas(self, mask):
         #TODO: describe all areas, assign id for each description
-        #TODO: description should have x,y and size and id.
         #TODO: Remove countours that should not be there. 21, and 22 countours not more or less.
         img,contours,hierarchy = cv2.findContours(mask.copy(),cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
         print(len(contours))
-
-        cv2.imshow("C", self._visualize(contours))
         return contours
 
-    def _visualize(self, contours):
-        #unified = []
-        #cont = np.vstack(ctn for ctn in contours)
-        #hull =  cv2.convexHull(cont)
-        #unified.append(hull)
-        blank_image = np.zeros((480,640,3), np.uint8)
-        l = [Utility.get_centroid(ctn) for ctn in contours]
-        for c in l:
-            cv2.circle(blank_image, c, 5, (200, 200 ,0))
-        cv2.circle(blank_image, Utility.get_avg_pos(contours), 5, (0, 200, 200))
-        cv2.imshow("dkjfjd", blank_image)
-        #cv2.drawContours(blank_image,unified,-1,(0,255,0),2)
-        #cv2.drawContours(blank_image, contours, -1, (0,0,255), 1)
-        return blank_image
 
     def _identify_bullseye(self, descriptions):
         avg_x, avg_y = Utility.get_avg_pos(descriptions)
         min_dist = float("inf")
         center = None
-        self._visualize(descriptions)
+
         for ctn in descriptions:
             x, y = Utility.get_centroid(ctn)
             dist = (avg_x - x)**2 + (avg_y-y)**2
@@ -80,7 +53,8 @@ class Board:
     def _fit_ellipse(self, contours):
         cont = np.vstack(ctn for ctn in contours)
         hull =  cv2.convexHull(cont)
-        return hull
+        ellipse = cv2.fitEllipse(hull)
+        return ellipse
 
     def _color_difference_segmentation(self, image):
         b,g,r = cv2.split(image)
