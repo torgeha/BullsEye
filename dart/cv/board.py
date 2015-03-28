3c72b477f4ab233ad67be9bbf16c6dd3d886d3c4
Olav Vatne <olavvatne@gmail.com>
2015-03-28 11:10:48 +0100

    board struture for board detection and segementing

---
+++ dart/cv/board.py
@@ -0,0 +1,19 @@
+
+class Board:
+
+    def detect(self, image):
+        '''
+        With the image supplied as argument, a shape description containing the board outline,
+        and bulleye coordinates.
+        '''
+        #TODO: Find outline
+        #TODO: Find bullseye center
+        #TODO: Find red and green point areas.
+        #TODO: Create a shape dscription and return it
+        pass
+
+    def _color_difference_segmentation(self, image):
+        pass
+
+    def _outline_segmentation(self, image):
+        pass
