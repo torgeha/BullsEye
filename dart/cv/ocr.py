be4139226a3416a7392a681d5de2d7f73378cef8
Olav Vatne <olavvatne@gmail.com>
2015-03-31 14:18:14 +0200

    Refactor of ocr into static methods

--- dart/cv/ocr.py
+++ dart/cv/ocr.py
@@ -1,28 +1,31 @@
 import cv2
 import numpy as np
+import scipy
 from utility import Utility
 from board import Board
 import math
 import sys
-
+print(np.__version__)
+from sklearn import neighbors
 class DartLearner:
 
     def __init__(self, samples=None, responses=None):
-        #self.model = cv2.KNearest() #TODO: cv2 Does not include ml in 3.0 yet
+        self.number = 20
+        self.model = neighbors.KNeighborsClassifier(self.number, weights='distance')
         if samples and responses:
             self.train(samples, responses)
 
-    def train(self, samples, responses):
-        self.model.train(samples, responses) #TODO: Change
+    def train(self, samples, targets):
+        self.model.fit(samples, targets)
         return self.model
 
     def init_trained_model(self, data):
         #TODO: Should be a way to pre train the model for to avoid online learning
         pass
 
-    def test(self, image):
+    def test(self, image, ellipse):
         #Let model classify training example
-        mask = DartLearner.create_mask(image, ellipse)
+        mask = DartHelper.create_mask(image, ellipse)
         img,contours,hierarchy = cv2.findContours(mask,cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
         groups = DartHelper.group_numbers(contours)
         for n in groups:
@@ -34,13 +37,14 @@ class DartLearner:
                 [x,y,w,h] = cv2.boundingRect(s)
                 if  h>10:
                     cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),2)
-                    roi = mask[y:y+h,x:x+w]
-                    roismall = cv2.resize(roi,(10,10))
-                    roismall = roismall.reshape((1,100))
-                    roismall = np.float32(roismall)
-                    retval, results, neigh_resp, dists = self.model.find_nearest(roismall, k = 1)
-                    string = str(int((results[0][0])))
-                    cv2.putText(image,string,(x,y+h),0,1,(0,255,0))
+                    roi = DartHelper.create_roi(mask, [x,y,w,h])
+                    roi = DartHelper.reshape_roi(roi)
+                    result = self.model.predict(roi)
+                    string = str(int((result)))
+                    print(string)
+                    cv2.putText(image,string,(x,y+h),0,1,(20,255,20))
+        cv2.imshow("Result", image)
+        cv2.waitKey(-1)
 class DartHelper:
     WIDE_FACTOR = 1.25
 
@@ -60,13 +64,13 @@ class DartHelper:
         return number_mask
 
     @staticmethod
-    def group_numbers(contours):
+    def group_numbers(contours, max_factor=.5):
         groups = [] #20 groups ideally
         avg_area = 0
         for cnt in contours:
             avg_area += cv2.contourArea(cnt)
         avg_area / len(contours) #TODO: or 20?
-        max_dist = np.sqrt(avg_area) * .5
+        max_dist = np.sqrt(avg_area) * max_factor
         if len(contours)>1:
             x,y = Utility.get_centroid(contours[0])
             groups.append([(x,y, contours[0])])
@@ -85,6 +89,17 @@ class DartHelper:
             return groups
 
     @staticmethod
+    def create_roi(mask, rectangle, width=10, height=10):
+        x,y,w,h = rectangle
+        roi = mask[y:y+h,x:x+w]
+        roismall = cv2.resize(roi,(width,height))
+        return roismall
+
+    @staticmethod
+    def reshape_roi(roi, length=100):
+        return np.float32(roi.reshape((1,length)))
+
+    @staticmethod
     def get_avg_group(group):
         sx = sum(x[0] for x in group)
         sy = sum(y[1] for y in group)
@@ -94,6 +109,8 @@ class DartHelper:
 class DartTrainingDataCreator:
     SAMPLE_FILENAME = "generalsamples.data"
     RESPONSE_FILENAME = "generalresponses.data"
+    ENTER = 13
+    ESCAPE = 27
 
     def sample(self, image, ellipse, sample_file=SAMPLE_FILENAME, response_file=RESPONSE_FILENAME):
         mask = DartHelper.create_mask(image, ellipse)
@@ -115,6 +132,8 @@ class DartTrainingDataCreator:
         for n in groups:
             avg_area = 0
             keys = [i for i in range(48,58)]
+            entered_keys = []
+            roi = None
             a = np.vstack(c[2] for c in n)
             avg_area =  [cv2.contourArea(c) for c in a]
             if avg_area >20:
@@ -122,16 +141,20 @@ class DartTrainingDataCreator:
                 [x,y,w,h] = cv2.boundingRect(s)
                 if  h>10:
                     cv2.rectangle(image,(x,y),(x+w,y+h),(0,0,255),2)
-                    roi = mask[y:y+h,x:x+w]
-                    roismall = cv2.resize(roi,(10,10))
                     cv2.imshow('norm',image)
-                    key = cv2.waitKey(0)
-                    key2 = cv2.waitKey(0)
-                if key == 27:  # (escape to quit)
-                    sys.exit()
-                elif key in keys and key2 in keys:
-                    responses.append(int(str(chr(key) + chr(key2))))
-                    sample = roismall.reshape((1,100))
+
+                    while True:
+                        key = cv2.waitKey(0)
+                        if  key is DartTrainingDataCreator.ENTER:
+                            break
+                        entered_keys.append(key)
+
+                if len(entered_keys)>0 and (k in keys for k in entered_keys):
+                    n = int(''.join(map(chr, entered_keys)))
+                    responses.append(n)
+                    roi = DartHelper.create_roi(mask, [x,y,w,h])
+                    sample = DartHelper.reshape_roi(roi)
+                    print(len(sample))
                     samples = np.append(samples,sample,0)
                     print(responses)
         responses = np.array(responses,np.float32)
@@ -145,12 +168,12 @@ class DartTrainingDataCreator:
 
 train = True
 
+t = "C:\Users\Olav\OneDrive for Business\BullsEye\Pictures\dart4.png"
+img = cv2.imread(t, 1)
+b = Board()
+ellipse = b.detect_ellipse(img)
 if train:
     learner = DartTrainingDataCreator()
-    b = Board()
-    t = "C:\Users\Olav\OneDrive for Business\BullsEye\Pictures\dartboard.png"
-    img = cv2.imread(t, 1)
-    ellipse = b.detect_ellipse(img)
     samples, responses = learner.sample(img, ellipse)
 else:
     samples = np.loadtxt('generalsamples.data',np.float32)
@@ -158,4 +181,4 @@ else:
     responses = responses.reshape((responses.size,1))
     learner = DartLearner()
     learner.train(samples, responses)
-    learner.test() #TODO: img
+    learner.test(img, ellipse) #TODO: img
